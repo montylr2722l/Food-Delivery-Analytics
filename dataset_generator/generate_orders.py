47607b56ac31c8530d0aaa2bb1fed471efec6319
montylr2722l <vishu.869600@gmail.com>
2026-06-26 19:06:34 +0530

    generate order dataset

--- dataset_generator/generate_orders.py
+++ dataset_generator/generate_orders.py
@@ -60,3 +60,109 @@ cities = [
 orders = []
 
 print("\nGenerating Orders...\n")
+
+# ======================================================
+# Helper Function: Generate Random Date
+# ======================================================
+
+def generate_order_date():
+    """
+    Returns a random date between
+    1 Jan 2025 and 31 Dec 2025
+    """
+
+    start_date = datetime(2025, 1, 1)
+    end_date = datetime(2025, 12, 31)
+
+    days = (end_date - start_date).days
+
+    return start_date + timedelta(days=random.randint(0, days))
+
+
+# ======================================================
+# Helper Function: Generate Order Time
+# ======================================================
+
+def generate_order_time():
+
+    hour_probability = random.random()
+
+    # Breakfast
+    if hour_probability < 0.10:
+        hour = random.randint(7, 10)
+
+    # Lunch Rush
+    elif hour_probability < 0.45:
+        hour = random.randint(12, 15)
+
+    # Evening
+    elif hour_probability < 0.60:
+        hour = random.randint(16, 18)
+
+    # Dinner Rush
+    elif hour_probability < 0.95:
+        hour = random.randint(19, 22)
+
+    # Night
+    else:
+        hour = random.randint(23, 24) % 24
+
+    minute = random.randint(0, 59)
+    second = random.randint(0, 59)
+
+    return hour, minute, second
+
+# ======================================================
+# Generate Orders
+# ======================================================
+
+for order_id in range(1, TOTAL_ORDERS + 1):
+
+    restaurant = restaurants.sample(1).iloc[0]
+
+    driver = drivers.sample(1).iloc[0]
+
+    order_date = generate_order_date()
+
+    hour, minute, second = generate_order_time()
+
+    order_datetime = order_date.replace(
+        hour=hour,
+        minute=minute,
+        second=second
+    )
+
+    orders.append({
+
+        "OrderID": order_id,
+
+        "CustomerID": random.randint(100000, 999999),
+
+        "RestaurantID": restaurant["RestaurantID"],
+
+        "DriverID": driver["DriverID"],
+
+        "OrderDate": order_datetime.strftime("%Y-%m-%d"),
+
+        "OrderTime": order_datetime.strftime("%H:%M:%S"),
+
+        "City": restaurant["City"]
+
+    })
+
+    if order_id % 10000 == 0:
+        print(f"{order_id} Orders Generated...")
+
+        # ======================================================
+# Save CSV
+# ======================================================
+
+orders_df = pd.DataFrame(orders)
+
+orders_df.to_csv(OUTPUT_FILE, index=False)
+
+print("\nOrders.csv Generated Successfully!")
+
+print(f"\nTotal Orders : {len(orders_df)}")
+
+print(f"\nSaved At : {OUTPUT_FILE}")
